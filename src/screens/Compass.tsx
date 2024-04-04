e275332e88b5f277e7779eaff98b94cc7cade6c2
Enzo Avagliano <eavagliano@laprovence.com>
2024-04-04 19:18:31 +0200

    Added compass

--- src/screens/Compass.tsx
+++ src/screens/Compass.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { Keyboard, Text, TouchableWithoutFeedback, View } from 'react-native'
+import { Image, Keyboard, Text, TouchableWithoutFeedback, View } from 'react-native'
 import { globalStyles } from '../styles/global'
 import { compassStyles } from '../styles/screens/compass'
-import { Magnetometer } from 'expo-sensors';
+import { Magnetometer, Gyroscope } from 'expo-sensors';
 import PageTitle from '../components/commons/PageTitle'
 import { getAngle } from '../helpers/scripts/compass/getAngle';
 import { getDirection } from '../helpers/scripts/compass/getDirection';
@@ -11,24 +11,33 @@ import { app_colors } from '../helpers/constants';
 
 export default function Compass({ navigation }: any) {
 
-  const [subscription, setSubscription] = useState<any>(null);
+  const [magnetometerSubscription, setMagnetometerSubscription] = useState<any>(null);
+  const [gyroscopeSubscription, setGyroscopeSubscription] = useState<any>(null);
   const [magnetometer, setMagnetometer] = useState<any>(0);
+  const [gyroscope, setGyroscope] = useState<any>(0);
   const degrees = Array(360).fill(null);
 
   const _subscribe = () => {
-    setSubscription(
+    setMagnetometerSubscription(
       Magnetometer.addListener(result => {
         setMagnetometer(getAngle(result));
       })
     );
 
-    // Magnetometer.setUpdateInterval(60000);
-    Magnetometer.setUpdateInterval(200);
+    setGyroscopeSubscription(
+      Gyroscope.addListener(result => {
+        setGyroscope(result);
+      })
+    )
+    Magnetometer.setUpdateInterval(500);
+    Gyroscope.setUpdateInterval(1000);
   };
 
   const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
+    magnetometerSubscription && magnetometerSubscription.remove();
+    gyroscopeSubscription && gyroscopeSubscription.remove();
+    setMagnetometerSubscription(null);
+    setGyroscopeSubscription(null);
   };
 
   useEffect(() => {
@@ -42,7 +51,7 @@ export default function Compass({ navigation }: any) {
         <PageTitle navigation={navigation} title='Boussole' />
         <View style={compassStyles.content}>
           <View style={compassStyles.content.compass}>
-            {
+            {/* {
               degrees.map((currentDegree, i) => {
                 return (
                   <View key={i} style={{
@@ -51,15 +60,29 @@ export default function Compass({ navigation }: any) {
                     left: 145,
                     width: 10,
                     height: 1,
-                    backgroundColor: i === getDegree(magnetometer) ? app_colors.red : app_colors.white,
+                    backgroundColor: app_colors.white,
                     transform: [{ rotate: `${i + 90}deg` }, {translateX: -145}]
                   }}/>
                 )
               })
-            }
+            } */}
+            <Image source={require('../../assets/icons/compassFrame.png')} resizeMode='contain' style={{ position: 'absolute', height: 300, width: 300, transform: [{ rotate: `-${getDegree(magnetometer)}deg`}] }}/>
+            <Image source={require('../../assets/icons/compassNeedle.png')} resizeMode='contain' style={{ position: 'absolute', height: 300, width: 300 }}/>
+            {/* <View style={[compassStyles.content.compass.mainNeedle, {transform: [{ rotate: `${getDegree(magnetometer) + 90}deg` }, { translateX: -145 }],}]}/> */}
             <View style={compassStyles.content.header}>
               <Text style={compassStyles.content.header.title}>{getDirection(getDegree(magnetometer))}</Text>
               <Text style={compassStyles.content.header.title}>{getDegree(magnetometer)}°</Text>
+            </View>
+          </View>
+          <View style={compassStyles.content.gyroscope}>
+            <Text style={{ color: 'white' }}>x: {Math.floor(gyroscope.x)}</Text>
+            <Text style={{ color: 'white' }}>y: {Math.floor(gyroscope.y)}</Text>
+            <Text style={{ color: 'white' }}>z: {Math.floor(gyroscope.z)}</Text>
+            {/* Create a fake horizon to make a gyroscope visualisation */}
+            <View style={{ height: 150, width: 300, backgroundColor: app_colors.white, position: 'absolute', top: 75 }}>
+              <View style={{ height: 1, width: 300, backgroundColor: app_colors.red, position: 'absolute', top: 75 }}/>
+              <View style={{ height: 1, width: 300, backgroundColor: app_colors.red, position: 'absolute', top: 150 }}/>
+              <View style={{ height: 1, width: 300, backgroundColor: app_colors.red, position: 'absolute', top: 225 }} />
             </View>
           </View>
         </View>
--- src/styles/screens/compass.ts
+++ src/styles/screens/compass.ts
@@ -6,22 +6,37 @@ export const compassStyles = {
     alignItems: 'center' as 'center',
 
     header: {
+      marginTop: 20,
       alignItems: 'center' as 'center',
       justifyContent: 'center' as 'center',
       height: '100%' as '100%',
 
       title: {
-        fontSize: 35,
+        fontSize: 30,
         color: app_colors.white,
         fontFamily: 'GilroyBlack',
       }
     },
 
     compass: {
-      marginTop: 20,
       height: 300,
       width: 300,
       borderRadius: 300,
+
+      mainNeedle: {
+        position: 'absolute' as 'absolute',
+        top: 150,
+        left: 145,
+        width: 150,
+        height: 1,
+        backgroundColor: app_colors.red,
+        transformOrigin: '0 0'
+      }
+    },
+
+    gyroscope: {
+      height: 300,
+      width: 300,
     }
   }
 }
