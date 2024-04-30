121e59447e60a4968dd8f7aaa14b4925df7faa5b
Enzo Avagliano <eavagliano@laprovence.com>
2024-04-30 09:47:24 +0200

    Updated city coords api call to Astroshare API

--- src/helpers/api/getCityCoords.ts
+++ src/helpers/api/getCityCoords.ts
@@ -1,9 +1,11 @@
+import axios from "axios"
 import { geocodingApi } from "."
 
 export const getCityCoords = async (cityName: string) => {
   try {
-    const response = await geocodingApi.get(``, { params: { q: cityName } })
-    return response.data
+    const response = await axios.get(`${process.env.EXPO_PUBLIC_ASTROSHARE_API_URL}/location/coords`, { params: { name: cityName } })
+    
+    return response.data.data
   } catch (error: any) {
     console.log("Get city coords error :", error.message)
     return error
