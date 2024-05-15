ec3c96f39fae3ebb0fc42f314cb4185fb17a4a13
Enzo Avagliano <eavagliano@laprovence.com>
2024-05-16 00:27:56 +0200

    Minor wording changes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -58,9 +58,9 @@ export default function Home({ navigation }: any) {
           <Text style={globalStyles.sections.subtitle}>Votre caisse à outils personnalisée</Text>
           <View style={homeStyles.toolsSuggestions.buttons}>
             <BigButton navigation={navigation} targetScreen={routes.weather} text="Météo en direct" subtitle="// C'est le moment de sortir le téléscope !" icon={require('../../assets/icons/FiSun.png')} />
-            <BigButton navigation={navigation} targetScreen={routes.compass} text="Boussole & Niveau" subtitle='// Pour une mise en station précise' icon={require('../../assets/icons/FiCompass.png')} />
+            <BigButton navigation={navigation} targetScreen={routes.compass} text="Boussole" subtitle='// Pour une mise en station précise' icon={require('../../assets/icons/FiCompass.png')} />
             <BigButton navigation={navigation} targetScreen={routes.moonPhases} text="Phases de la Lune" subtitle='// Calculez les phases de la Lune' icon={require('../../assets/icons/FiMoon.png')} />
-            <BigButton disabled navigation={navigation} targetScreen={routes.solarWeather} text="Météo solaire et aurores" subtitle="// La météo de notre étoile" icon={require('../../assets/icons/SolarWind.png')} />
+            <BigButton disabled navigation={navigation} targetScreen={routes.solarWeather} text="Météo solaire et aurores" subtitle="// Bientôt disponible !" icon={require('../../assets/icons/SolarWind.png')} />
           </View>
         </View>
         <View style={homeStyles.nasaTools}>
