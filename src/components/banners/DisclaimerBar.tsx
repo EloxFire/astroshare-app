dede502e37bd01bec76f00428223b0abec820383
Enzo Avagliano <eavagliano@laprovence.com>
2024-05-19 12:57:29 +0200

    Added Video support for APOD module

--- src/components/banners/DisclaimerBar.tsx
+++ src/components/banners/DisclaimerBar.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { disclaimerBarStyles } from '../../styles/components/banners/disclaimerBar'
 import { app_colors } from '../../helpers/constants'
 
 interface DisclaimerBarProps {
   message: string
   type: 'error' | 'info'
+  onPress?: () => void
 }
 
-export default function DisclaimerBar({ message, type }: DisclaimerBarProps) {
+export default function DisclaimerBar({ message, type, onPress }: DisclaimerBarProps) {
   const bgColor = type === 'error' ? app_colors.red_eighty : app_colors.white_no_opacity
 
   return (
-    <View style={[disclaimerBarStyles.bar, {backgroundColor: bgColor}]}>
-      {type === 'error' && <Image source={require('../../../assets/icons/FiXCircle.png')} style={[disclaimerBarStyles.bar.icon, {opacity: 1}]} />}
-      {type === 'info' && <Image source={require('../../../assets/icons/FiInfo.png')} style={disclaimerBarStyles.bar.icon} />}
-      <Text style={[disclaimerBarStyles.bar.text, {opacity: type === 'error' ? 1 : .5 }]}>{message}</Text>
-    </View>
+    <TouchableOpacity disabled={!onPress} onPress={onPress}>
+      <View style={[disclaimerBarStyles.bar, {backgroundColor: bgColor}]}>
+        {type === 'error' && <Image source={require('../../../assets/icons/FiXCircle.png')} style={[disclaimerBarStyles.bar.icon, {opacity: 1}]} />}
+        {type === 'info' && <Image source={require('../../../assets/icons/FiInfo.png')} style={disclaimerBarStyles.bar.icon} />}
+        <Text style={[disclaimerBarStyles.bar.text, {opacity: type === 'error' ? 1 : .5 }]}>{message}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
--- src/screens/Apod.tsx
+++ src/screens/Apod.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Dimensions, Image, ScrollView, Text, View } from 'react-native'
 import { globalStyles } from '../styles/global'
 import { apodStyles } from '../styles/screens/apod'
 import { showToast } from '../helpers/scripts/showToast'
 import { APODPicture } from '../helpers/types/APODPicture'
+import { app_colors } from '../helpers/constants'
+import { ResizeMode, Video } from 'expo-av'
+import * as Clipboard from 'expo-clipboard'
 import axios from 'axios'
 import Toast from 'react-native-root-toast'
 import PageTitle from '../components/commons/PageTitle'
-import { app_colors } from '../helpers/constants'
 import dayjs from 'dayjs'
+import DisclaimerBar from '../components/banners/DisclaimerBar'
 
 export default function Apod({ navigation }: any) {
 
   const [apod, setApod] = useState<APODPicture | null>(null)
+  const videoRef = useRef(null);
   
   useEffect(() => {
     getApod()
@@ -29,6 +33,11 @@ export default function Apod({ navigation }: any) {
     }
   }
 
+  const clickLink = async () => {
+    await Clipboard.setStringAsync(apod?.url || '');
+    showToast({ message: 'Lien copié', duration: Toast.durations.LONG, type: 'success' });
+  }
+
   return (
     <View style={globalStyles.body}>
       <PageTitle navigation={navigation} title="APOD" subtitle="// Image du jour de la NASA" />
@@ -36,7 +45,7 @@ export default function Apod({ navigation }: any) {
       <ScrollView>
         <View style={apodStyles.content}>
           <Text style={apodStyles.content.title}>{ apod?.title.replace(/(\r\n|\n|\r)/gm, "") || "Chargement"}</Text>
-          <Text style={[apodStyles.content.text, {color: app_colors.white_eighty}]}>Copyright : {apod?.copyright.replace(/(\r\n|\n|\r)/gm, "") || "Chargement"}</Text>
+          {apod?.copyright && <Text style={[apodStyles.content.text, {color: app_colors.white_eighty}]}>Copyright : {apod?.copyright.replace(/(\r\n|\n|\r)/gm, "") || "Chargement"}</Text>}
           <Text style={[apodStyles.content.text, {color: app_colors.white_eighty, marginTop: 5}]}>Date : {apod?.date ? dayjs(apod?.date).format('DD/MM/YYYY') : "Chargement"}</Text>
           {
             apod?.media_type === 'image' && (
@@ -44,9 +53,23 @@ export default function Apod({ navigation }: any) {
             )
           }
           {
-            apod?.media_type === 'video' && (
-              <Text style={[apodStyles.content.text, {color: app_colors.white_eighty}]}>Vidéo non supportée...</Text>
-            )
+            apod?.media_type === 'video' &&
+              apod?.url.includes('youtube') ?
+                <View style={{display: 'flex', flexDirection:'column', gap: 10, marginVertical: 30, width: '100%'}}>
+                  <DisclaimerBar message='Les vidéos provenant de Youtube ne sont pas encore supportées. Cliquez sur le lien ci-dessous pour copier le lien de la vidéo.' type='error' />
+                  <DisclaimerBar message={apod?.url} type='info' onPress={() => clickLink()} />
+                </View>
+                :
+                <Video
+                  ref={videoRef}
+                  source={{uri: apod?.url || ''}}
+                  isMuted={true}
+                  rate={1.0}
+                  shouldPlay={true}
+                  isLooping={true}
+                  resizeMode={ResizeMode.CONTAIN}
+                  style={{ width: Dimensions.get('screen').width, height: Dimensions.get('screen').width, marginVertical: 10}}
+                />
           }
           <Text style={apodStyles.content.subtitle}>Description :</Text>
           <Text style={[apodStyles.content.text, {fontSize: 20, alignSelf: 'flex-start', lineHeight: 28}]}>{apod?.explanation || 'Chargement'}</Text>
