727712bde4bdeec958711ce885678b6e4e072db9
Enzo Avagliano <eavagliano@laprovence.com>
2024-05-21 13:54:20 +0200

    Added image loader

--- src/screens/SolarWeather.tsx
+++ src/screens/SolarWeather.tsx
@@ -7,23 +7,24 @@ import axios from 'axios'
 import { solarWeatherStyles } from '../styles/screens/solarWeather'
 import dayjs from 'dayjs'
 import { ESunFilter } from '../helpers/types/SunFilter'
-import { sunIMageFiltersDescription, sunImagesSrcWavelengths } from '../helpers/constants'
+import { app_colors, sunIMageFiltersDescription, sunImagesSrcWavelengths } from '../helpers/constants'
 import SimpleButton from '../components/commons/buttons/SimpleButton'
 
 export default function SolarWeather({ navigation }: any) {
 
+  const [loadingImage, setLoadingImage] = useState<boolean>(true)
   const [currentImageFilter, setCurrentImageFilter] = useState<ESunFilter>('HMIIC' as ESunFilter)
   const [currentImageUri, setCurrentImageUri] = useState<string>(sunImagesSrcWavelengths[currentImageFilter] as string)
   const [currentImageDate, setCurrentImageDate] = useState<string>('')
 
   useEffect(() => {
-    console.log('Current image filter:', currentImageFilter);
-    
     (async () => {
+      setLoadingImage(true)
       const response = await axios.get(sunImagesSrcWavelengths[currentImageFilter])
       console.log('Response:', dayjs(response.request.responseHeaders['Date']).format('DD/MM/YYYY HH:mm:ss'));
       setCurrentImageDate(dayjs(response.request.responseHeaders['Date']).format('DD/MM/YYYY HH:mm:ss'))
       setCurrentImageUri(sunImagesSrcWavelengths[currentImageFilter])
+      setLoadingImage(false)
     })()
   }, [currentImageFilter])
   
@@ -38,7 +39,7 @@ export default function SolarWeather({ navigation }: any) {
           <Text style={solarWeatherStyles.container.subtitle}>Image capturée le : {currentImageDate}</Text>
           <Text style={[solarWeatherStyles.container.subtitle, {marginTop: 8}]}>Filtre : {currentImageFilter}</Text>
           <Text style={solarWeatherStyles.container.subtitle}>Zone d'étude : {sunIMageFiltersDescription[currentImageFilter]}</Text>
-          <Image source={{ uri: currentImageUri }} style={solarWeatherStyles.sunImage} />
+          <Image source={{ uri: currentImageUri }} style={[solarWeatherStyles.sunImage, {opacity: loadingImage ? .1 : 1, borderWidth: loadingImage ? 1 : 0, borderColor: app_colors.white_eighty}]} />
           <View style={solarWeatherStyles.container.buttons}>
             {
               Object.keys(ESunFilter).map((key: string) => {
