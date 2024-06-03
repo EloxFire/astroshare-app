faff2dfb4db0b35a36505d195bfecb513f248a2e
Enzo Avagliano <eavagliano@laprovence.com>
2024-06-03 18:29:34 +0200

    Scope alignment tool started

--- src/components/Compass.tsx
+++ src/components/Compass.tsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 import {
   Image,
-  Keyboard,
   Text,
-  TouchableWithoutFeedback,
   View,
 } from "react-native";
-import { globalStyles } from "../styles/global";
 import { compassStyles } from "../styles/screens/compass";
 import { Magnetometer, Gyroscope } from "expo-sensors";
-import PageTitle from "../components/commons/PageTitle";
 import { getAngle } from "../helpers/scripts/compass/getAngle";
 import { getDirection } from "../helpers/scripts/compass/getDirection";
 import { getDegree } from "../helpers/scripts/compass/getDegrees";
-import { app_colors } from "../helpers/constants";
 
 export default function Compass({ navigation }: any) {
   const [magnetometerSubscription, setMagnetometerSubscription] =
@@ -52,35 +47,27 @@ export default function Compass({ navigation }: any) {
   }, []);
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={globalStyles.body}>
-        <PageTitle navigation={navigation} title="Boussole" subtitle="// Pour une mise en station précise" />
-        <View style={globalStyles.screens.separator} />
-        <View style={compassStyles.content}>
-          <View style={compassStyles.content.compass}>
-            <Image
-              source={require("../../assets/icons/compassFrame.png")}
-              resizeMode="contain"
-              style={[compassStyles.content.compass.dialImage,{
-                transform: [{ rotate: `-${getDegree(magnetometer)}deg` }],
-              }]}
-            />
-            <Image
-              source={require("../../assets/icons/compassNeedle.png")}
-              resizeMode="contain"
-              style={{ position: "absolute", height: 300, width: 300 }}
-            />
-            <View style={compassStyles.content.header}>
-              <Text style={compassStyles.content.header.title}>
-                {getDirection(getDegree(magnetometer))}
-              </Text>
-              <Text style={compassStyles.content.header.title}>
-                {getDegree(magnetometer)}°
-              </Text>
-            </View>
-          </View>
-        </View>
+    <View style={compassStyles.content.compass}>
+      <Image
+        source={require("../../assets/icons/compassFrame.png")}
+        resizeMode="contain"
+        style={[compassStyles.content.compass.dialImage,{
+          transform: [{ rotate: `-${getDegree(magnetometer)}deg` }],
+        }]}
+      />
+      <Image
+        source={require("../../assets/icons/compassNeedle.png")}
+        resizeMode="contain"
+        style={{ position: "absolute", height: 300, width: 300 }}
+      />
+      <View style={compassStyles.content.header}>
+        <Text style={compassStyles.content.header.title}>
+          {getDirection(getDegree(magnetometer))}
+        </Text>
+        <Text style={compassStyles.content.header.title}>
+          {getDegree(magnetometer)}°
+        </Text>
       </View>
-    </TouchableWithoutFeedback>
+    </View>
   );
 }
--- src/screens/ScopeAlignment.tsx
+++ src/screens/ScopeAlignment.tsx
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
-  Image,
   Keyboard,
-  Text,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import { compassStyles } from "../styles/screens/compass";
-import { Magnetometer, Gyroscope } from "expo-sensors";
 import PageTitle from "../components/commons/PageTitle";
-import { getAngle } from "../helpers/scripts/compass/getAngle";
-import { getDirection } from "../helpers/scripts/compass/getDirection";
-import { getDegree } from "../helpers/scripts/compass/getDegrees";
-import { app_colors } from "../helpers/constants";
 
 export default function ScopeAlignment({ navigation }: any) {
 
