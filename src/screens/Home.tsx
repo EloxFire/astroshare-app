77f02ac6215b732a42d034ab4dbd7f4a102bcb77
Enzo Avagliano <eavagliano@laprovence.com>
2024-07-14 18:10:10 +0200

    Reverted to ISS only satellite tracker

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -93,7 +93,7 @@ export default function Home({ navigation }: any) {
           <Text style={globalStyles.sections.subtitle}>Explorez toujours plus !</Text>
           <View style={homeStyles.nasaTools.buttons}>
             <ToolButton disabled={!hasInternetConnection} navigation={navigation} targetScreen={routes.apod.path} text="APOD" subtitle="// Image du jour de la NASA" image={require('../../assets/images/tools/apod.png')} />
-            <ToolButton disabled={!hasInternetConnection} navigation={navigation} targetScreen={routes.satelliteTracker.path} text="Satellite Tracker" subtitle="// Informations ISS et Starlink" image={require('../../assets/images/tools/isstracker.png')} />
+            <ToolButton disabled={!hasInternetConnection} navigation={navigation} targetScreen={routes.satelliteTracker.path} text="ISS Tracker" subtitle="// Position de l'ISS en temps réel" image={require('../../assets/images/tools/isstracker.png')} />
             {/* <ToolButton disabled={!hasInternetConnection} navigation={navigation} targetScreen={routes.skymapgenerator.path} text="Sky map" subtitle="// Générez des cartes du ciel" image={require('../../assets/images/tools/skymap.png')} /> */}
           </View>
         </View>
--- src/styles/screens/satelliteTracker.ts
+++ src/styles/screens/satelliteTracker.ts
@@ -3,7 +3,7 @@ import { app_colors } from "../../helpers/constants";
 
 export const satelliteTrackerStyles = {
   map: {
-    width: Dimensions.get('window').width,
+    width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height,
     paddingTop: 100,
   },
@@ -19,49 +19,36 @@ export const satelliteTrackerStyles = {
     padding: 10,
     paddingTop: StatusBar.currentHeight
   },
-  tools: {
+  button: {
     position: 'absolute' as 'absolute',
     display: 'flex' as 'flex',
-    flexDirection: 'row' as 'row',
+    flexDirection: 'column' as 'column',
     alignItems: 'center' as 'center',
     justifyContent: 'center' as 'center',
-    gap: 20,
-    width: Dimensions.get('window').width,
-    height: 90,
-    bottom: 100,
+    top: StatusBar.currentHeight! + 100,
+    left: 0,
+    zIndex: 1,
+    width: 50,
+    height: 40,
     backgroundColor: app_colors.black,
-    borderTopColor: app_colors.white_eighty,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderColor: app_colors.white_eighty,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
     borderTopWidth: 1,
-    paddingHorizontal: 25,
 
-    button: {
-      borderRadius: 10,
-      display: 'flex' as 'flex',
-      alignItems: 'center' as 'center',
-      justifyContent: 'center' as 'center',
-      padding: 5,
-      gap: 5,
-      
-      icon: {
-        width: 24,
-        height: 24,
-      },
-
-      label: {
-        color: app_colors.white,
-        fontSize: 10,
-        fontFamily: 'AuxMono',
-        textTransform: 'uppercase' as 'uppercase',
-      }
+    centerIss: {
+      top: StatusBar.currentHeight! + 150,
     }
   },
   issModal: {
     position: 'absolute' as 'absolute',
-    bottom: 140,
+    bottom: 40,
     left: 0,
     zIndex: 1,
     height: 'auto' as 'auto',
-    width: Dimensions.get('window').width - 20,
+    width: Dimensions.get('screen').width - 20,
     marginHorizontal: 10,
     backgroundColor: app_colors.black,
     borderRadius: 10,
