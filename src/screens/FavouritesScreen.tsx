e6b6f7bc242bb35fdcb4c7a9828d79a3cae3f739
Enzo Avagliano <eavagliano@laprovence.com>
2024-08-05 20:05:39 +0200

    Fix

--- src/screens/FavouritesScreen.tsx
+++ src/screens/FavouritesScreen.tsx
@@ -18,6 +18,7 @@ export default function FavouritesScreen({ navigation }: any) {
   useEffect(() => {
     (async () => {
       const favs = await getObject(storageKeys.favouriteObjects)
+      if (!favs) return
       setObjects(favs)
     })()
   }, [isFocused])
