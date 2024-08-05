5c513d18039885b1784287878920f3a4adf6efd3
Enzo Avagliano <eavagliano@laprovence.com>
2024-08-06 00:37:22 +0200

    New planets icons + version code

---
+++ src/helpers/scripts/utils/regex/searchRegex.ts
@@ -0,0 +1,11 @@
+export const planetRegex = /\b[Pp][Ll][Aa][Nn][EeÉé][Tt][Ee]?[Ss]?\b/g;
+
+export const mercuryRegex = /\b[Mm][Ee][Rr][Cc][Uu][Rr][Yy]?[Ee]?[Ss]?\b/g;
+export const venusRegex = /\b[Vv][Ee][Nn][Uu][Ss][Uu]?[Ss]?\b/g;
+export const earthRegex = /\b[Ee][Aa]?[Rr]?[Tt]?[Hh]?\b|\b[Tt][Ee][Rr][Rr][Ee]?[Ss]?\b/g;
+export const marsRegex = /\b[Mm][Aa][Rr][Ss][Uu]?[Ss]?\b/g;
+export const jupiterRegex = /\b[Jj][Uu][Pp][Ee][Rr][Tt][Uu]?[Ss]?\b/g;
+export const saturnRegex = /\b[Ss][Aa][Tt][Uu][Rr][Nn][Uu]?[Ss]?\b/g;
+export const uranusRegex = /\b[Uu][Rr][Aa][Nn][Uu][Ss][Uu]?[Ss]?\b/g;
+export const neptuneRegex = /\b[Nn][Ee][Pp][Tt][Uu][Nn][Ee]?[Ss]?\b/g;
+export const plutoRegex = /\b[Pp][Ll][Uu][Tt][Oo]?[Ss]?\b/g;
