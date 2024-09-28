93b8f2862384f980f9dcbe153b59da8da48cbbb4
Enzo Avagliano <eavagliano@laprovence.com>
2024-09-28 17:28:09 +0200

    Version code

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   name: "Astroshare",
   slug: "astroshare-app",
-  version: "v1.2.0",
+  version: "v1.3.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
@@ -37,7 +37,7 @@ module.exports = {
       }
     },
     package: "fr.eavagliano.astroshare",
-    versionCode: 28
+    versionCode: 29
   },
   web: {
     favicon: "./assets/favicon.png"
