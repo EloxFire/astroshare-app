5d6e08ed56c7e7f65b233afe9d41c9cb48f7c7c3
Enzo Avagliano <eavagliano@laprovence.com>
2024-10-08 16:56:02 +0200

    Added sections in settings screen

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import { globalStyles } from "../styles/global";
 import { settingsStyles } from "../styles/screens/settings";
 import { useSettings } from "../contexts/AppSettingsContext";
@@ -23,16 +23,19 @@ export default function Settings({ navigation }: any) {
       <View style={globalStyles.screens.separator} />
       <ScrollView>
         <View style={settingsStyles.content}>
+          <Text style={[settingsStyles.content.categoryTitle, {marginTop: 0}]}>{i18n.t('settings.categories.general')}</Text>
           <BigButton icon={require('../../assets/icons/FiEye.png')} text={i18n.t('settings.buttons.nightMode.title')} subtitle={i18n.t('settings.buttons.nightMode.subtitle')} hasCheckbox isChecked={isNightMode} onPress={() => handleNightMode()} />
-          <BigButton icon={require('../../assets/icons/FiFileText.png')} text={i18n.t('settings.buttons.changelog.title')} subtitle={i18n.t('settings.buttons.changelog.subtitle')} navigation={navigation} targetScreen={routes.changelogScreen.path} />
-          {/* <BigButton disabled icon={require('../../assets/icons/FiWifi.png')} text={i18n.t('settings.buttons.cellularData.title')} subtitle={i18n.t('settings.buttons.cellularData.subtitle')} hasCheckbox isChecked={isCellularDataEnabled} onPress={() => handleCellularData()} /> */}
-          <BigButton icon={require('../../assets/icons/FiViewPoint.png')} text={i18n.t('settings.buttons.favoritesViewPoints.title')} subtitle={i18n.t('settings.buttons.favoritesViewPoints.subtitle')} navigation={navigation} targetScreen={routes.favoritesViewPoints.path} />
-          <BigButton icon={require('../../assets/icons/FiWidget.png')} text={i18n.t('settings.buttons.widgets.title')} subtitle={i18n.t('settings.buttons.widgets.subtitle')} navigation={navigation} targetScreen={routes.widgetsManager.path} />
-          <BigButton icon={require('../../assets/icons/FiLifeBuoy.png')} text={i18n.t('settings.buttons.tutorial.title')} subtitle={i18n.t('settings.buttons.tutorial.subtitle')} targetScreen={routes.tutorial.path} navigation={navigation} />
-          <BigButton icon={require('../../assets/icons/FiDataInspect.png')} text={i18n.t('settings.buttons.data.title')} subtitle={i18n.t('settings.buttons.data.subtitle')} targetScreen={routes.astroDataInfos.path} navigation={navigation} />
           <BigButton icon={require('../../assets/icons/FiTranslation.png')} text={i18n.t('settings.buttons.language.title')} subtitle={i18n.t('settings.buttons.language.subtitle')} targetScreen={routes.language.path} navigation={navigation} />
-          <BigButton icon={require('../../assets/icons/FiShield.png')} text={i18n.t('settings.buttons.permissions.title')} subtitle={i18n.t('settings.buttons.permissions.subtitle')} onPress={() => Linking.openSettings()} />
+          <BigButton icon={require('../../assets/icons/FiLifeBuoy.png')} text={i18n.t('settings.buttons.tutorial.title')} subtitle={i18n.t('settings.buttons.tutorial.subtitle')} targetScreen={routes.tutorial.path} navigation={navigation} />
+          <Text style={settingsStyles.content.categoryTitle}>{i18n.t('settings.categories.appearance')}</Text>
+          <BigButton icon={require('../../assets/icons/FiWidget.png')} text={i18n.t('settings.buttons.widgets.title')} subtitle={i18n.t('settings.buttons.widgets.subtitle')} navigation={navigation} targetScreen={routes.widgetsManager.path} />
+          <BigButton icon={require('../../assets/icons/FiViewPoint.png')} text={i18n.t('settings.buttons.favoritesViewPoints.title')} subtitle={i18n.t('settings.buttons.favoritesViewPoints.subtitle')} navigation={navigation} targetScreen={routes.favoritesViewPoints.path} />
+          <Text style={settingsStyles.content.categoryTitle}>{i18n.t('settings.categories.infos')}</Text>
           <BigButton icon={require('../../assets/icons/FiInfo.png')} text={i18n.t('settings.buttons.about.title')} subtitle={i18n.t('settings.buttons.about.subtitle')} navigation={navigation} targetScreen={routes.about.path} />
+          <BigButton icon={require('../../assets/icons/FiFileText.png')} text={i18n.t('settings.buttons.changelog.title')} subtitle={i18n.t('settings.buttons.changelog.subtitle')} navigation={navigation} targetScreen={routes.changelogScreen.path} />
+          <BigButton icon={require('../../assets/icons/FiDataInspect.png')} text={i18n.t('settings.buttons.data.title')} subtitle={i18n.t('settings.buttons.data.subtitle')} targetScreen={routes.astroDataInfos.path} navigation={navigation} />
+          <BigButton icon={require('../../assets/icons/FiShield.png')} text={i18n.t('settings.buttons.permissions.title')} subtitle={i18n.t('settings.buttons.permissions.subtitle')} onPress={() => Linking.openSettings()} />
+          {/* <BigButton disabled icon={require('../../assets/icons/FiWifi.png')} text={i18n.t('settings.buttons.cellularData.title')} subtitle={i18n.t('settings.buttons.cellularData.subtitle')} hasCheckbox isChecked={isCellularDataEnabled} onPress={() => handleCellularData()} /> */}
         </View>
       </ScrollView>
     </View>
--- src/styles/screens/settings.ts
+++ src/styles/screens/settings.ts
@@ -5,5 +5,15 @@ export const settingsStyles = {
     marginTop: 20,
     display: 'flex' as 'flex',
     flexDirection: 'column' as 'column',
+
+    categoryTitle: {
+      fontSize: 16,
+      color: app_colors.white,
+      marginBottom: 5,
+      marginTop: 10,
+      opacity: .5,
+      fontFamily: 'GilroyBlack',
+      textTransform: 'uppercase' as 'uppercase',
+    }
   }
 };
