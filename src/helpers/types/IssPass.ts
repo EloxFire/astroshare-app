1b9a3356351b617148eafa83216316dd16a204a3
Enzo Avagliano <eavagliano6@gmail.com>
2024-11-22 01:13:06 +0100

    WIP

---
+++ src/helpers/types/IssPass.ts
@@ -0,0 +1,16 @@
+export interface IssPass {
+  startAz: number;
+  startAzCompass: string;
+  startEl: number;
+  startUTC: number;
+  maxAz: number;
+  maxAzCompass: string;
+  maxEl: number;
+  maxUTC: number;
+  endAz: number;
+  endAzCompass: string;
+  endEl: number;
+  endUTC: number;
+  mag: number;
+  duration: number;
+}
