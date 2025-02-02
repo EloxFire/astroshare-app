3d4465526b43eaa8ef739e18a2e263f17863e329
Enzo Avagliano <eavagliano6@gmail.com>
2025-02-02 18:07:51 +0100

    Fix onboarding screen scroll issue for small devices or big fonts

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Image, Text, TouchableOpacity, View } from 'react-native'
+import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native'
 import { globalStyles } from '../styles/global'
 import { comingSoonStyles } from '../styles/screens/cominSoon'
 import { routes } from '../helpers/routes'
@@ -17,16 +17,16 @@ export default function Onboarding({ navigation }: any) {
 
   const { refreshCurrentUserLocation } = useSettings()
 
-  useEffect(() => {
-    (async () => {
-      const firstTime = await isFirstLaunch();
-      if (!firstTime) {
-        navigation.dispatch(
-          StackActions.replace(routes.home.path)
-        );
-      }
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async () => {
+  //     const firstTime = await isFirstLaunch();
+  //     if (!firstTime) {
+  //       navigation.dispatch(
+  //         StackActions.replace(routes.home.path)
+  //       );
+  //     }
+  //   })()
+  // }, [])
 
   const changeLocale = async (code: string) => {
     i18n.locale = code
@@ -44,30 +44,32 @@ export default function Onboarding({ navigation }: any) {
 
   return (
     <View style={globalStyles.body}>
-      <Image source={require('../../assets/logos/astroshare_logo_white.png')} resizeMode="contain" style={{ alignSelf: "center", height: 50, marginBottom: 20 }} />
-      <Text style={onboardingStyles.title}>{i18n.t('onboarding.title')}</Text>
-      <Text style={onboardingStyles.subtitle}>{i18n.t('onboarding.subtitle')}</Text>
-      <Text style={[onboardingStyles.subtitle, { marginBottom: 40 }]}>{i18n.t('onboarding.disclaimer')}</Text>
-      <Text style={onboardingStyles.text}>{i18n.t('onboarding.text')}</Text>
-      <View style={{ marginTop: 30 }}>
-        <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText')}</Text>
-        <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText2')}</Text>
-        <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText3')}</Text>
-        <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText4')}</Text>
-        <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText5')}</Text>
-      </View>
-      <Text style={[onboardingStyles.text, { marginTop: 30 }]}>{i18n.t('onboarding.messageShown')}</Text>
-      <TouchableOpacity style={comingSoonStyles.button} onPress={() => handleAccept()} disabled={!isFirstLaunch}>
-        <Text style={comingSoonStyles.buttonText}>{i18n.t('onboarding.accept')}</Text>
-      </TouchableOpacity>
-      <View style={{ marginTop: 20, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-        <TouchableOpacity style={[languageSelectionStyles.content.button, { justifyContent: 'center' }]} onPress={() => changeLocale('fr')}>
-          <Text style={languageSelectionStyles.content.button.icon}>{getUnicodeFlagIcon('FR')}</Text>
+      <ScrollView>
+        <Image source={require('../../assets/logos/astroshare_logo_white.png')} resizeMode="contain" style={{ alignSelf: "center", height: 50, marginBottom: 20 }} />
+        <Text style={onboardingStyles.title}>{i18n.t('onboarding.title')}</Text>
+        <Text style={onboardingStyles.subtitle}>{i18n.t('onboarding.subtitle')}</Text>
+        <Text style={[onboardingStyles.subtitle, { marginBottom: 40 }]}>{i18n.t('onboarding.disclaimer')}</Text>
+        <Text style={onboardingStyles.text}>{i18n.t('onboarding.text')}</Text>
+        <View style={{ marginTop: 30 }}>
+          <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText')}</Text>
+          <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText2')}</Text>
+          <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText3')}</Text>
+          <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText4')}</Text>
+          <Text style={onboardingStyles.listText}>{i18n.t('onboarding.listText5')}</Text>
+        </View>
+        <Text style={[onboardingStyles.text, { marginTop: 30 }]}>{i18n.t('onboarding.messageShown')}</Text>
+        <TouchableOpacity style={comingSoonStyles.button} onPress={() => handleAccept()} disabled={!isFirstLaunch}>
+          <Text style={comingSoonStyles.buttonText}>{i18n.t('onboarding.accept')}</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[languageSelectionStyles.content.button, { justifyContent: 'center' }]} onPress={() => changeLocale('en')}>
-          <Text style={languageSelectionStyles.content.button.icon}>{getUnicodeFlagIcon('GB')}</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={{ marginTop: 20, marginBottom: 80, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity style={[languageSelectionStyles.content.button, { justifyContent: 'center' }]} onPress={() => changeLocale('fr')}>
+            <Text style={languageSelectionStyles.content.button.icon}>{getUnicodeFlagIcon('FR')}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[languageSelectionStyles.content.button, { justifyContent: 'center' }]} onPress={() => changeLocale('en')}>
+            <Text style={languageSelectionStyles.content.button.icon}>{getUnicodeFlagIcon('GB')}</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   )
 }
