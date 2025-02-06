09353b3369657efdda2e234de7a92ae33e8726f8
Etienne R <Etienne.raymond5@laposte.net>
2025-02-06 13:10:45 +0100

    Camera Rework!
    Locked camera on alt/az axis using quaternions

--- src/screens/skymap/Planetarium.tsx
+++ src/screens/skymap/Planetarium.tsx
@@ -38,7 +38,8 @@ pointergeometry.setAttribute('position', new THREE.Float32BufferAttribute(pointe
 const pointermaterial = createPointerMaterial();
 const pointerUI = new THREE.Points(pointergeometry, pointermaterial);
 pointerUI.visible = false;
-
+let azAngle = 0;
+let altAngle = Math.PI/2;
 
 export default function Planetarium({ navigation }: any) {
 
@@ -148,7 +149,16 @@ export default function Planetarium({ navigation }: any) {
     ground.renderOrder = 100;
     scene.add(ground);
 
-    camera.lookAt(getGlobePosition(currentUserLocation.lat, currentUserLocation.lon));
+    let q11 = new THREE.Quaternion;
+    let q12 = new THREE.Quaternion;
+    let q13 = new THREE.Quaternion;
+    let Y1 = new THREE.Vector3(0, 0, 1);
+    let X1 = new THREE.Vector3(1, 0, 0);
+    q11.setFromAxisAngle(Y1, 0);
+    q12.setFromAxisAngle(X1, Math.PI / 2);
+    ground.getWorldQuaternion(q13);
+    let qtot1 = q13.multiply(q11).multiply(q12);
+    camera.setRotationFromQuaternion(qtot1.normalize());
 
     // Animation loop to render the scene
     const animate = () => {
@@ -183,12 +193,12 @@ export default function Planetarium({ navigation }: any) {
 
 
   const onShowEqGrid = () => {
-    if(showEqGrid){
+    if (showEqGrid) {
       sceneRef.current?.remove(EquatorialGrid.grid1);
       sceneRef.current?.remove(EquatorialGrid.grid2);
       sceneRef.current?.remove(EquatorialGrid.grid3);
       setShowEqGrid(false);
-    }else{
+    } else {
       sceneRef.current?.add(EquatorialGrid.grid1);
       sceneRef.current?.add(EquatorialGrid.grid2);
       sceneRef.current?.add(EquatorialGrid.grid3);
@@ -197,12 +207,12 @@ export default function Planetarium({ navigation }: any) {
   }
 
   const onShowAzGrid = () => {
-    if(showAzGrid){
+    if (showAzGrid) {
       sceneRef.current?.remove(AzimuthalGrid.grid1);
       sceneRef.current?.remove(AzimuthalGrid.grid2);
       sceneRef.current?.remove(AzimuthalGrid.grid3);
       setShowAzGrid(false);
-    }else{
+    } else {
       sceneRef.current?.add(AzimuthalGrid.grid1);
       sceneRef.current?.add(AzimuthalGrid.grid2);
       sceneRef.current?.add(AzimuthalGrid.grid3);
@@ -210,20 +220,20 @@ export default function Planetarium({ navigation }: any) {
     }
   }
   const onShowConstellations = () => {
-    if(showConstellations){
+    if (showConstellations) {
       sceneRef.current?.remove(Constellations);
       setShowConstellations(false);
-    }else{
+    } else {
       sceneRef.current?.add(Constellations);
       setShowConstellations(true);
     }
   }
 
   const onShowGround = () => {
-    if(showGround){
+    if (showGround) {
       sceneRef.current?.remove(ground);
       setShowGround(false);
-    }else{
+    } else {
       sceneRef.current?.add(ground);
       setShowGround(true);
     }
@@ -245,8 +255,25 @@ export default function Planetarium({ navigation }: any) {
     .onChange((e) => {
       const camera = cameraRef.current; // Access camera from ref
       if (camera) {
-        camera.rotateY(getEffectiveAngularResolution(camera.getEffectiveFOV(), cameraWidth) * (e.translationX - oldX));
-        camera.rotateX(getEffectiveAngularResolution(camera.getEffectiveFOV(), cameraWidth) * (e.translationY - oldY));
+        let q1 = new THREE.Quaternion;
+        let q2 = new THREE.Quaternion;
+        let q3 = new THREE.Quaternion;
+        let newAzAngle = azAngle + getEffectiveAngularResolution(camera.getEffectiveFOV(), cameraWidth) * (e.translationX - oldX);
+        let Y = new THREE.Vector3(0, 0, 1);
+        q1.setFromAxisAngle(Y, newAzAngle);
+        let X = new THREE.Vector3(1, 0, 0);
+        let newAltAngle = altAngle + getEffectiveAngularResolution(camera.getEffectiveFOV(), cameraWidth) * (e.translationY - oldY);
+        if (newAltAngle > Math.PI) {
+          newAltAngle = Math.PI;
+        } else if (newAltAngle < 0) {
+          newAltAngle = 0;
+        }
+        q2.setFromAxisAngle(X, newAltAngle);
+        ground.getWorldQuaternion(q3);
+        let qtot = q3.multiply(q1).multiply(q2);
+        camera.setRotationFromQuaternion(qtot.normalize());
+        azAngle = newAzAngle;
+        altAngle = newAltAngle;
         oldX = e.translationX;
         oldY = e.translationY;
         Vx = e.velocityX;
@@ -263,8 +290,27 @@ export default function Planetarium({ navigation }: any) {
   const Inertia = () => {
     const camera = cameraRef.current;
     if (camera) {
-      camera.rotateY(getEffectiveAngularResolution(camera.getEffectiveFOV(), camWdth) * Vx * 0.01);
-      camera.rotateX(getEffectiveAngularResolution(camera.getEffectiveFOV(), camWdth) * Vy * 0.01);
+      let q1 = new THREE.Quaternion;
+      let q2 = new THREE.Quaternion;
+      let q3 = new THREE.Quaternion;
+      let newAzAngle = azAngle + getEffectiveAngularResolution(camera.getEffectiveFOV(), camWdth) * Vx * 0.01;
+      let Y = new THREE.Vector3(0, 0, 1);
+      q1.setFromAxisAngle(Y, newAzAngle);
+      let X = new THREE.Vector3(1, 0, 0);
+      let newAltAngle = altAngle + getEffectiveAngularResolution(camera.getEffectiveFOV(), camWdth) * Vy * 0.01;
+      if (newAltAngle > Math.PI) {
+        newAltAngle = Math.PI;
+      } else if (newAltAngle < 0) {
+        newAltAngle = 0;
+      }
+      q2.setFromAxisAngle(X, newAltAngle);
+      ground.getWorldQuaternion(q3);
+      let qtot = q3.multiply(q1).multiply(q2);
+      camera.setRotationFromQuaternion(qtot.normalize());
+      azAngle = newAzAngle;
+      altAngle = newAltAngle;
+      // camera.rotateY(getEffectiveAngularResolution(camera.getEffectiveFOV(), camWdth) * Vx * 0.01);
+      // camera.rotateX(getEffectiveAngularResolution(camera.getEffectiveFOV(), camWdth) * Vy * 0.01);
       Vx = Vx * 0.98;
       Vy = Vy * 0.98;
       if (Math.abs(Vx) < 0.1) {
@@ -368,9 +414,9 @@ export default function Planetarium({ navigation }: any) {
         let index = '0';
         if (typeof intersects[0] !== 'undefined') {
           intersects.forEach((value, i) => {
-            if (Vmin > parseFloat(starsCatalog[intersects[i].index!.toString()].V)){
-              index=intersects[i].index!.toString();
-              Vmin=parseFloat(starsCatalog[intersects[i].index!.toString()].V);
+            if (Vmin > parseFloat(starsCatalog[intersects[i].index!.toString()].V)) {
+              index = intersects[i].index!.toString();
+              Vmin = parseFloat(starsCatalog[intersects[i].index!.toString()].V);
             }
           })
           let pointerCoos = convertSphericalToCartesian(0.5, parseFloat(starsCatalog[index].ra), parseFloat(starsCatalog[index].dec));
@@ -388,28 +434,28 @@ export default function Planetarium({ navigation }: any) {
       }
     });
 
-  const gestures = Gesture.Simultaneous(pinch, rotation, pan);
+  const gestures = Gesture.Simultaneous(pinch, pan);
   const taps = Gesture.Exclusive(singleTap);
   const composed = Gesture.Race(gestures, taps);
 
   return (
-      <GestureHandlerRootView>
-        <PlanetariumUI
-          navigation={navigation}
-          infos={currentTapInfos}
-          onShowAzGrid={onShowAzGrid}
-          onShowConstellations={onShowConstellations}
-          onShowEqGrid={onShowEqGrid}
-          onShowGround={onShowGround}
-          onShowPlanets={() => {}}
-          onShowDSO={() => {}}
-          onCenterObject={() => {}}
-        />
-        <GestureDetector gesture={composed}>
-          <View style={planetariumStyles.container}>
-            <GLView style={{ flex: 1 }} onContextCreate={_onContextCreate} />
-          </View>
-        </GestureDetector>
-      </GestureHandlerRootView>
+    <GestureHandlerRootView>
+      <PlanetariumUI
+        navigation={navigation}
+        infos={currentTapInfos}
+        onShowAzGrid={onShowAzGrid}
+        onShowConstellations={onShowConstellations}
+        onShowEqGrid={onShowEqGrid}
+        onShowGround={onShowGround}
+        onShowPlanets={() => { }}
+        onShowDSO={() => { }}
+        onCenterObject={() => { }}
+      />
+      <GestureDetector gesture={composed}>
+        <View style={planetariumStyles.container}>
+          <GLView style={{ flex: 1 }} onContextCreate={_onContextCreate} />
+        </View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
