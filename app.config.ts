4c0436fea66a75f98fae1f89b62051fc17c1ab81
Enzo Avagliano <eavagliano6@gmail.com>
2025-11-22 14:00:49 +0100

    VERSION CODE

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   name: "Astroshare",
   slug: "astroshare-app",
-  version: "v2.2.0",
+  version: "v2.3.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
