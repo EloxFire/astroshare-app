47f05fcce6118d87cc353865d6bcdd9f0558e7fc
Enzo Avagliano <eavagliano6@gmail.com>
2026-08-25 20:09:14 +0200

    chore: Update software keyboard layout mode to resize

--- app.config.ts
+++ app.config.ts
@@ -76,7 +76,7 @@ module.exports = {
     package: IS_DEV
       ? "fr.eavagliano.astroshare.dev"
       : "fr.eavagliano.astroshare",
-    softwareKeyboardLayoutMode: "padding",
+    softwareKeyboardLayoutMode: "resize",
   },
   web: {
     favicon: "./assets/favicon.png"
